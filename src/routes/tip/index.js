c1daec503b5e617be1cc75253b4a4da64887c420
habib-153 <h.r.sihab155@gmail.com>
2024-02-06 22:32:37 +0600

    route created for tips

--- src/routes/tip/index.js
+++ src/routes/tip/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { createTip } = require('../../api/v1/tip/createTip');
+const { getTip } = require('../../api/v1/tip/getTip');
 const router = express.Router()
 
 router.post('/tips', createTip)
+router.get('/tips', getTip)
 
 module.exports = router
