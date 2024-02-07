0bac8fe6b8bc68f435e399cf0ed89df7c07bd95e
Mohammed Rifatul Islam <mdrifatulislam08@gmail.com>
2024-02-07 17:13:19 +0600

    chat

--- src/app.js
+++ src/app.js
@@ -12,9 +12,19 @@ const commentRoutes = require('./routes/comments/index')
 const appointmentRoutes = require('./routes/appointment/index')
 const tipsRoutes = require('./routes/tip/index')
 
+const port = process.env.PORT || 5000
 
 const app = express()
-const port = process.env.PORT || 5000
+
+// const http=require("http");
+// const cors = require("cors");
+// const socketIO = require("socket.io");
+// const users=[{}];
+
+// app.use(cors());
+// const server=http.createServer(app);
+
+// const io=socketIO(server);
 
 applyMiddleware(app);
 
@@ -30,9 +40,7 @@ app.use(commentRoutes)
 app.use(appointmentRoutes)
 app.use(tipsRoutes)
 
-
 const main = async () => {
-
     // connecting to database
    // console.log("Connecting to database...");
     await connectDB();
@@ -40,15 +48,40 @@ const main = async () => {
     app.listen(port, () => {
         console.log(`Server is running on port: ${port}`)
     })
-
 }
 
 main();
 
 
+
 app.get('/health', (req, res) => {
     res.send('Server is running.')
 })
+
+
+// io.on("connection",(socket)=>{
+//     console.log("New Connection");
+
+//     socket.on('joined',({user})=>{
+//           users[socket.id]=user;
+//           console.log(`${user} has joined `);
+//           socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+//           socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+//     })
+
+//     socket.on('message',({message,id})=>{
+//         io.emit('sendMessage',{user:users[id],message,id});
+//     })
+
+//     socket.on('disconnected',()=>{
+//           socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+//         console.log(`user left`);
+//     })
+// });
+
+
+
+
 
 app.all('*', (req, res, next) => {
     const error = new Error(`The requested url is invalid:  [${req.url}] `)
