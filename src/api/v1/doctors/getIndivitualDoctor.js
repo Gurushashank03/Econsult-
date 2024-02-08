1eb2aaff0dd20b7f992459bff985541c0a2339f2
habib-153 <h.r.sihab155@gmail.com>
2024-02-08 15:42:27 +0600

    api created doctor role and doctor profile and dashboard for doctors

---
+++ src/api/v1/doctors/getIndivitualDoctor.js
@@ -0,0 +1,12 @@
+const Doctors = require("../../../models/Doctors");
+
+const getIndividualDoctor = async (req, res) => {
+    const email = req.params.email
+    const query = { email: email}
+    console.log(query)
+    const data = await Doctors.findOne(query)
+    console.log(data)
+    res.send(data)
+}
+
+module.exports = { getIndividualDoctor };
--- src/routes/Doctor/index.js
+++ src/routes/Doctor/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const { createDoctors } = require('../../api/v1/doctors/createDoctors')
+const { doctorRole } = require('../../api/v1/doctors/doctorRole')
+const { getIndividualDoctor } = require('../../api/v1/doctors/getIndivitualDoctor')
+const { updateDoctor } = require('../../api/v1/doctors/updateDoctor')
 const router = express.Router()
 
 router.post('/doctors/createDoctors',createDoctors)
+router.get("/doctors/:email" , doctorRole)
+router.get("/doctor/:email" , getIndividualDoctor)
+router.put('/doctor/:email',updateDoctor)
+
 
 module.exports = router
