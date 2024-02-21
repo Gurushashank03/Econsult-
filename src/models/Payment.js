1736a0db85d279caba9f7a1bdbe3b348c8b1f23f
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-02-21 11:41:30 +0600

    payment sestem bakcend and payment add data

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -1,5 +1,52 @@
 const {Schema, model} = require("mongoose");
 
+const paymentSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    transactionId:{
+        type: String,
+        required: true
+    },
+    billPayDate:{
+        type: Date,
+        required: true
+    },
+    appointmentDate:{
+        type: String,
+        required: true
+    },
+    appointmentTime:{
+        type: String,
+        required: true
+    },
+    treatment:{
+        type: String,
+        required: true
+    },
+    appointmentId:{
+        type: String,
+        required: true
+    },
+    paidStatus: Boolean,
+  });
   
 const Payment = model('Payment', paymentSchema, 'payment');
 
