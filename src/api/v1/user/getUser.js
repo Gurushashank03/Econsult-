d61a4e8aa16c05b3226ee4ca1af81ec83b972cbb
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-03-01 15:26:59 +0600

    delete user from admin dashboard

--- src/api/v1/user/getUser.js
+++ src/api/v1/user/getUser.js
@@ -16,4 +16,31 @@ const getUser = async (req, res) => {
     res.send(data)
 }
 
-module.exports = { getUsers, getUser };
+const deleteUser = async (req, res) => {
+    const query = { email: req?.params?.email }
+    // console.log(query)
+    const data = await User.deleteOne(query);
+    res.send(data)
+}
+
+const updateUser = async (req, res) => {
+  const email= req.params.email;
+  const updatedUser = req.body;
+  const query = { email: email };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: {
+      name: updatedUser.name,
+      email: updatedUser.email,
+      loc: updatedUser.location,
+      blood_group: updatedUser.blood_group,
+      image: updatedUser.image,
+      password: updatedUser.password,
+      role: updatedUser.role,
+    },
+  };
+  const result = await Doctors.updateOne(query, updateDoc, options);
+  res.send(result);
+};
+
+module.exports = { getUsers, getUser, deleteUser, updateUser };
